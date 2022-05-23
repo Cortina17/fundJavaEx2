a990d1d37301fd7555d140ead51003a9a6026e0e
diego <diego.cortinagarcia@gmail.com>
2022-05-23 08:18:00 +0200

    created class cuenta

---
+++ src/fundJavaEx2/Cuenta.java
@@ -0,0 +1,23 @@
+package fundJavaEx2;
+
+public class Cuenta implements Transferible {
+
+	@Override
+	public void depositar(double cant) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void extraer(double cant) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mover(double cant, int nroCuenta) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
